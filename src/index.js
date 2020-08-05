220e1a59c05d20e53a83f0a78f68377c1b915473
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-08-05 16:40:36 -0700

    getting ready to have routing and redux

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import rootReducer from "./state/reducer";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('app')
+);
